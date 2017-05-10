37068a786922b4fe19bebe7b49f7c1343451886a
Suprabhat Gurrala <supragurrala@gmail.com>
2017-05-10 19:59:40 -0400

    Added seekbar and tap to roll functionality

--- app/src/main/java/com/paradice/suprabhat/paradice/DiceActivity.java
+++ app/src/main/java/com/paradice/suprabhat/paradice/DiceActivity.java
@@ -4,9 +4,12 @@ import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import java.util.Random;
+
+import static android.util.TypedValue.COMPLEX_UNIT_SP;
 
 public class DiceActivity extends AppCompatActivity {
 
@@ -16,6 +19,25 @@ public class DiceActivity extends AppCompatActivity {
         setContentView(R.layout.dice_layout);
 
         final TextView textView = (TextView) findViewById(R.id.textView);
+        final SeekBar seekBar = (SeekBar) findViewById(R.id.seekBar);
+        int numDice = 0;
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progressVal, boolean fromUser) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 
         ConstraintLayout constraintLayout = (ConstraintLayout) findViewById(R.id.dice_constraint_layout);
 
@@ -23,7 +45,8 @@ public class DiceActivity extends AppCompatActivity {
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        textView.setText(String.valueOf(APIClient.getRandomNumber(6)));
+                        textView.setText(APIClient.getMultipleDiceRolls(seekBar.getProgress() + 1));
+                        textView.setTextSize(COMPLEX_UNIT_SP, 96);
                     }
                 }
         );
