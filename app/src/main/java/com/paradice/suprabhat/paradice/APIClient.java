b8a00666e55e59003139df180a97159ae505d271
Suprabhat Gurrala <supragurrala@gmail.com>
2017-05-10 19:59:07 -0400

    Added methods to generate dice rolls

--- app/src/main/java/com/paradice/suprabhat/paradice/APIClient.java
+++ app/src/main/java/com/paradice/suprabhat/paradice/APIClient.java
@@ -15,4 +15,41 @@ public enum APIClient {
     public static int getRandomNumber(int maxVal) {
         return instance.rand.nextInt(maxVal);
     }
+
+    public static String getDiceRoll() {
+        int num = getRandomNumber(6);
+        String roll = "";
+        switch (num) {
+            case 0:
+                roll = "\u2680";
+                break;
+            case 1:
+                roll = "\u2681";
+                break;
+            case 2:
+                roll = "\u2682";
+                break;
+            case 3:
+                roll = "\u2683";
+                break;
+            case 4:
+                roll = "\u2684";
+                break;
+            case 5:
+                roll = "\u2685";
+                break;
+            default:
+                roll = "Tap to roll dice.";
+                break;
+        }
+        return roll;
+    }
+
+    public static String getMultipleDiceRolls(int num) {
+        String rolls = "";
+        for (int i = 0; i < num; i++) {
+            rolls += getDiceRoll();
+        }
+        return rolls;
+    }
 }
