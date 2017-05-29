9a2d176900f089cb7ca7f2f3a5757355230b80da
Suprabhat Gurrala <supragurrala@gmail.com>
2017-05-29 13:38:12 -0400

    Added a cache of Random.org data to allow for instant responses

--- app/src/main/java/com/paradice/suprabhat/paradice/Roller.java
+++ app/src/main/java/com/paradice/suprabhat/paradice/Roller.java
@@ -1,23 +1,68 @@
 package com.paradice.suprabhat.paradice;
 
+import org.random.api.RandomOrgCache;
+import org.random.api.RandomOrgClient;
+
+import java.util.NoSuchElementException;
 import java.util.Random;
 
 import static org.random.util.RandomOrgAPIKey.RANDOM_API_KEY;
 
+/**
+ * Class to generate random rolls. Defined as enum for Singleton design pattern.
+ */
 public enum Roller {
     instance;
-    Random rand;
+    int[] randomCache;
+    int index = 0;
+    Random rand = new Random();
 
-    private Roller() {
-        rand = new org.random.util.RandomOrgRandom(RANDOM_API_KEY);
+    /**
+     * Private constructor to initially populate cache.
+     */
+    Roller() {
+        populateCache();
     }
 
-    public static int getRandomNumber(int maxVal) {
-        return instance.rand.nextInt(maxVal);
+    /**
+     * Gets the next value available in the cache or repopulates cache if it has been used.
+     * @return next available value in cache.
+     */
+    public static int getRandomNumber() {
+        if (instance.index >= instance.randomCache.length) {
+            populateCache();
+            instance.index = 0;
+        }
+        return instance.randomCache[instance.index++];
     }
 
+    /**
+     * Populates cache using Random.org data. If data is not available, populates a temporary cache
+     * of pseudorandom numbers.
+     */
+    public static void populateCache() {
+        RandomOrgClient roc = RandomOrgClient.getRandomOrgClient(RANDOM_API_KEY);
+        RandomOrgCache<int[]> c = roc.createIntegerCache(1000, 0, 5);
+
+        int[] randoms;
+
+        try {
+             randoms = c.get();
+
+        } catch (NoSuchElementException e) {
+            randoms = new int[10];
+            Random rand = new Random();
+            randoms[0] = rand.nextInt(6);
+        }
+        instance.randomCache = randoms;
+    }
+
+    /**
+     * Converts integers to Unicode dice characters
+     * @return Unicode dice String representing the roll
+     */
     public static String getDiceRoll() {
-        int num = getRandomNumber(6);
+        int num = getRandomNumber();
         String roll = "";
         switch (num) {
             case 0:
@@ -45,6 +90,11 @@ public enum Roller {
         return roll;
     }
 
+    /**
+     * Gets multiple dice rolls at once.
+     * @param num number of dice rolls to retrieve
+     * @return a String of Unicode dice chars
+     */
     public static String getMultipleDiceRolls(int num) {
         String rolls = "";
         for (int i = 0; i < num; i++) {
